a1766ae37cae0ef7c4ffd31c229ce9ebdee1d48d
jnielsengamedev <jnielsengamedev@gmail.com>
2023-02-27 12:59:02 -0800

    add a new level with moving platforms

---
+++ Assets/jnielsen/Scripts/Data/Platform.cs
@@ -0,0 +1,17 @@
+namespace jnielsen.Scripts.Data
+{
+	public abstract class Platform
+	{
+		public enum Type
+		{
+			MoveForward,
+			SinePlatform
+		}
+
+		public enum MoveType
+		{
+			Vertical,
+			Horizontal
+		}
+	}
+}
