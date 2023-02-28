6eddfa316e3d62af413d1effc970a6f8f75d7023
MrPasimio <jpasimio@mvsd320.org>
2023-02-28 11:24:43 -0800

    Updated Game with 5 playable levels and all contributed assets

--- Assets/Lucas/Scripts/PlatForms/MovingPlatformLK.cs
+++ Assets/Lucas/Scripts/PlatForms/MovingPlatformLK.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingPlatform : MonoBehaviour
+public class MovingPlatformLK : MonoBehaviour
 {
     //current pos variables
     private float xPos;
